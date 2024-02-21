acee5bd4c8167f2cc86f2a2c1576b554a76a3a50
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-02-21 16:14:54 +0800

    feat: updated the navbar and included product's link

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from products.views import product_detail, ProductListView
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='products'),
+    path('', ProductListView.as_view(), name='products_list'),
     path('<slug:slug>/', product_detail, name='product_detail'),
 ]
