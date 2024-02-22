17726da955505fdfe60fe184a03edadee27f484e
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-02-23 07:27:46 +0800

    refactor: broke the listing_view into pagination & listing

--- products/filters.py
+++ products/filters.py
@@ -4,8 +4,6 @@ from .models import Product
 
 
 class ProductFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='icontains')
-
     class Meta:
         model = Product
         fields = {
