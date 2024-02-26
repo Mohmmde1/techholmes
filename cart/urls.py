c09a52b9a380f657c4ed442971fcf8701d2f667b
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-02-27 00:08:17 +0800

    feat: enabled the user to update the items in the cart

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from cart.views import add_to_cart
+from cart.views import add_to_cart, update_cart
 
 urlpatterns = [
     path('', add_to_cart, name='cart'),
+    path('update_cart', update_cart, name='update_cart')
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+
 from products.models import Product
 
 from .models import Cart, CartItem
@@ -35,7 +37,24 @@ def add_to_cart(request):
                 cart_item.quantity += quantity
             cart_item.save()
             return render(request, 'cart/cart.html', {"cart":cart})
-    else:
-        form = AddCartForm()
+
 
     return render(request, 'cart/cart.html', {"cart":cart})
+
+
+@login_required
+def update_cart(request):
+    cart = get_cart(request)
+
+    if request.method == 'POST':
+        for item in cart.items.all():
+            quantity_key = f'quantity_{item.id}'
+            if quantity_key in request.POST:
+                quantity = int(request.POST[quantity_key])
+                if quantity > 0:
+                    item.quantity = quantity
+                    item.save()
+                else:
+                    item.delete()
+
+    return HttpResponseRedirect('/cart/')
